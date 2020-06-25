dd84b343ba50cd2c1220c8d45dfa1eee0a2fc2a4
David Barbosa da Silva <daviscoinit@gmail.com>
2020-06-25 20:49:05 -0300

    Preparando Spring e JPA

---
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -0,0 +1,20 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+public class JPAConfiguration {
+	
+	@Bean
+	public void entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean = 
+				new LocalContainerEntityManagerFactoryBean();
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		
+		// Continue em 18min 
+	}
+}
