0f587d2a9496552cfd7d67040fe1f63c71585c31
David Barbosa da Silva <davidinteraction@outlook.com>
2020-07-03 13:04:26 -0300

    Preparando Controller, DAO e JSP

--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -11,10 +13,15 @@ import br.com.casadocodigo.loja.models.Produto;
 @Repository
 @Transactional
 public class ProdutoDAO {
-	
+
 	@PersistenceContext
 	private EntityManager manager;
+
 	public void gravar(Produto produto) {
 		manager.persist(produto);
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("Select p from Produto p", Produto.class).getResultList();
 	}
 }
